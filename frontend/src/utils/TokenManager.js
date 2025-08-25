fe583933b3a173a55dc11b7f3e412fb7ef1454a0
Dezeli <haterecursive@gmail.com>
2025-08-25 16:29:16 +0900

    refactor:ts->js

--- frontend/src/utils/TokenManager.js
+++ frontend/src/utils/TokenManager.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const ACCESS_TOKEN_KEY = 'accessToken';
 const REFRESH_TOKEN_KEY = 'refreshToken';
-const BASE_URL = 'http://localhost:8000/api/v1';
+// const BASE_URL = 'http://localhost:8000/api/v1';
+const BASE_URL = 'http://15.164.211.115/api/v1';
 
 class TokenManager {
   static instance = null;
