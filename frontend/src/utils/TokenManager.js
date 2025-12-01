62df590a027e0b82e52bcf2382581d7714572031
Dezeli <haterecursive@gmail.com>
2025-12-01 14:16:31 +0900

    ec2 settings

--- frontend/src/utils/TokenManager.js
+++ frontend/src/utils/TokenManager.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const ACCESS_TOKEN_KEY = 'accessToken';
 const REFRESH_TOKEN_KEY = 'refreshToken';
-const BASE_URL = 'http://localhost:8000/api/v1';
+// const BASE_URL = 'http://localhost:8000/api/v1';
 // const BASE_URL = 'http://15.164.211.115/api/v1';
+const BASE_URL = 'http://neighbiz.store/api/v1';
 
 class TokenManager {
   static instance = null;
